3dfcc2e31aa2e773ae9d1a21207b910ef35e5b52
DiegoDev20 <dediegosc@gmail.com>
2024-03-30 14:22:29 -0500

    Add ex06

--- ex06/ft_print_comb2.c
+++ ex06/ft_print_comb2.c
@@ -5,11 +5,30 @@ int main(void){
 	return(0);
 }
 void ft_print_comb2(void){
-	int num1=57,num2;
-	while(num1<=){
-		num2=0;
-		while(num2<=99){
-			
+	int num1=48,num2=48,num3=48,num4=48;
+	while(num1<=57 && num2<=57){
+		while(num3<=57){
+			while(num4<=57){
+				char nu1=num1;
+				char nu2=num2;
+				char nu3=num3;
+				char nu4=num4;
+				write(1,&nu1,1);
+				write(1,&nu2,1);
+				write(1," ",1);
+				write(1,&nu3,1);
+				write(1,&nu4,1);
+				write(1,";  ",1);
+				num4++;
+			}
+			num4=48;
+			num3++;
+		}
+		num3=48;
+		num2++;
+		if(num2==57){
+			num1++;
+			num2=48;
 		}
 	}
 }
